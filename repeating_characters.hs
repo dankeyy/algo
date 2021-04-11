c5632a02e6bc540287be807f18bc577619608c9f
dankeyy <dankeyy@protonmail.com>
2021-04-11 23:53:53 +0300

    refactor

--- repeating_characters.hs
+++ repeating_characters.hs
@@ -1,8 +1,8 @@
--- returns number of character repetitions. aaabbbbc -> 341 
+-- returns number of character repetitions. aaa bbbb c -> 341 
 import Data.List
 
 solve :: String -> String
-solve = concatMap (show. length) . group . concat . words
+solve = concatMap (show . length) . group . concat . words
 
 main :: IO ()
 main = getLine >>= (putStrLn . solve)
