5a20d90a00e29d21e1521f0c956bf867a48336ff
dankeyy <dankeyy@protonmail.com>
2021-08-06 23:14:43 +0300

    prettifying fb1 and adding a second solution

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,9 +1,29 @@
 
-fizzbuzz :: IO ()
-fizzbuzz = mapM_ putStrLn
+rules = [(3, "fizz"), (5, "buzz")]
+
+divisibleBy n m = mod n m == 0
+
+n <|> [] = show n
+_ <|> xs = concat xs
+----------------------------------------------------------------
+
+fb1 :: IO ()
+fb1 = mapM_ putStrLn
   [
-    max 
+    max
     (show x) 
-    (concat [fb | (n, fb) <- [(3, "Fizz"),(5, "Buzz")], x `mod` n == 0])
+    (concat [fizzbuzz | (n, fizzbuzz) <- rules, x `divisibleBy` n])
     | x <- [1..100]
   ]
+
+----------------------------------------------------------------
+
+fb2 :: Int -> String
+fb2 n = n <|> [ fizzbuzz | (r, fizzbuzz) <- rules, n `divisibleBy` r]
+
+
+main2 ::IO ()
+main2 = mapM_ (putStrLn . fb2) [1..100]
+
+----------------------------------------------------------------
+
