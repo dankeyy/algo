e4c56a3b3194b7e5d8a583c86a3099457f0ce956
dankeyy <dankeyy@protonmail.com>
2021-08-07 14:48:08 +0300

    generalized and added a 3rd solution

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,25 +1,34 @@
 
+rules :: [(Int, String)]
 rules = [(3, "fizz"), (5, "buzz")]
 
+
+divisibleBy :: Integral a => a -> a -> Bool
 divisibleBy n m = mod n m == 0
 
-n <|> [] = show n
-_ <|> xs = concat xs
+infix 0 <|>
+(<|>) :: [a] -> [a] -> [a]
+n <|> [] = n
+_ <|> xs = xs
+
 ----------------------------------------------------------------
 
-fb1 :: IO ()
-fb1 = mapM_ putStrLn
+fb1 :: [String]
+fb1 = 
   [
     max
     (show n) 
     (concat [fizzbuzz | (r, fizzbuzz) <- rules, n `divisibleBy` r])
-    | n <- [1..100]
+    | n <- [1..]
   ]
+
+main13 :: IO ()
+main13 = mapM_ putStrLn $ take 100 fb1
 
 ----------------------------------------------------------------
 
 fb2 :: Int -> String
-fb2 n = n <|> [ fizzbuzz | (r, fizzbuzz) <- rules, n `divisibleBy` r]
+fb2 n = show n <|> concat [ fizzbuzz | (r, fizzbuzz) <- rules, n `divisibleBy` r ]
 
 
 main2 :: IO ()
@@ -27,3 +36,8 @@ main2 = mapM_ (putStrLn . fb2) [1..100]
 
 ----------------------------------------------------------------
 
+fb3 :: [String]
+fb3 = zipWith3 (\n f b -> n <|> (f ++ b))
+              (map show [1..])
+              (cycle ["", "", "fizz"]) 
+              (cycle ["", "", "", "", "buzz"]) 
