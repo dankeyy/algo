3a3589762c748878faee0b91cd61ba4c8b0fdc6f
dankeyy <74069206+dankeyy@users.noreply.github.com>
2021-08-28 08:24:52 +0300

    added doc and comments

--- longest_incr_subseq.py
+++ longest_incr_subseq.py
@@ -2,7 +2,8 @@
 from operator import itemgetter
 
 def longest_incresing_subseq(x):
-
+    "finding the longest subsequence of increasing & following elements in the list, returning the length of that subseq along with its first index"
+    
     longest = initiated = 0
     s_table = (x[i+1] - x[i] == 1 for i in range(len(x) - 1))
 
@@ -21,9 +22,4 @@ def longest_incresing_subseq(x):
 
 x = [7,6,9,10,11,15,16,12,5,6,7,8,9,7,2,3,4]
 print(max(longest_incresing_subseq(x), key=itemgetter(0)))
-
-# max_length, starting_at = 
-# print(f'{max_length=}, {starting_at=}') 
-
-# [(0, False), (1, False), (2, True), (3, True), (4, False), (5, True), (6, False), (7, False), (8, True), (9, True), 
-# (10, True), (11, True), (12, False), (13, False), (14, True), (15, True), (16, True), (17, True), (18, True)]
+# outputs (5 -length, 8 - starting index) because 5,6,7,8,9 is the longest increasing subseq, starting from 8 with a length of 5 
