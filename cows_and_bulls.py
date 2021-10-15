767906d41d10c6193793725f875eb3b30e80d395
dankeyy <dankeyy@protonmail.com>
2021-10-15 22:54:31 +0300

    solved

---
+++ cows_and_bulls.py
@@ -0,0 +1,20 @@
+from random import sample
+from string import digits
+
+def main():
+    random_num = ''.join(sample(digits, 4))
+    bulls = 0
+
+    while bulls != 4:
+        bulls = cows = 0
+        guess = input() # trust worthy 4 distinct digit number, im not gonna bother checking
+         
+        for user_digit, random_digit in zip(guess, random_num):
+            if user_digit == random_digit: bulls += 1
+            elif user_digit in random_num: cows += 1
+
+        print(f"{bulls = }, {cows = }")
+
+    print('u win')
+
+main()
