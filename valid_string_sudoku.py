e58e550cc7da55e681c086cbbe0766828fa1f206
dankeyy <dankeyy@protonmail.com>
2021-11-09 21:21:40 +0200

    cleaning up

--- valid_string_sudoku.py
+++ valid_string_sudoku.py
@@ -8,7 +8,7 @@ def fields(s):
 
     for i in range(9):
         yield s[i :: 9] # columns # or- m[x][i] for x in range(len(grid))
-        yield m[i] # rows # or- yield s[i*9 : (i+1) * 9]
+        yield m[i] # rows # or- s[i*9 : (i+1) * 9]
 
         x0, y0 = map(mul3, divmod(i, 3))
         yield [m[x0+x][y0+y] for x, y in product(range(3), repeat=2)] # maybe as gen/ set comp instead idk
@@ -17,7 +17,6 @@ def fields(s):
 def valid_sudoku(s):
     return all(len(set(f)) == 9 for f in fields(s))
 
-# valid board
+# valid board for testing
 test = '864371259325849761971265843436192587198657432257483916689734125713528694542916378'
-
 print(valid_sudoku(test))
