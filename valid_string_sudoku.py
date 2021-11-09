67facfd715fe411132942fc8e32cd85be1daeee9
dankeyy <dankeyy@protonmail.com>
2021-11-09 21:55:06 +0200

    looks prettier in yield from

--- valid_string_sudoku.py
+++ valid_string_sudoku.py
@@ -7,11 +7,15 @@ def fields(s):
     m = [s[i:i+9] for i in range(0, len(s), 9)]
 
     for i in range(9):
-        yield s[i :: 9] # columns # or- m[x][i] for x in range(len(grid))
-        yield m[i] # rows # or- s[i*9 : (i+1) * 9]
-
         x0, y0 = map(mul3, divmod(i, 3))
-        yield [m[x0+x][y0+y] for x, y in product(range(3), repeat=2)] # maybe as gen/ set comp instead idk
+
+        yield from (
+            s[i :: 9], # column
+            m[i], # row
+            [m[x0+x][y0+y] for x, y in product(range(3), repeat=2)], # cube
+        )
+    # column can also be represented as m[x][i] for x in range(len(grid))
+    # row can also be represented as s[i*9 : (i+1) * 9]
 
 
 def valid_sudoku(s):
